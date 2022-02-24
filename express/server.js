83892ef2d8901056b16a1eada47b6ff02587542d
Aditya Giri <dtrg21@gmail.com>
2022-02-24 10:53:02 +0530

    Add handlebars

--- express/server.js
+++ express/server.js
@@ -1,23 +1,36 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+var hbs = require("express-hbs");
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine(
+  "hbs",
+  hbs.express4({
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+router.get("/", (req, res) => {
+  res.render("Home", {
+    message: "Greetings from express server from Aditya",
+  });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-app.use('/another', (req, res) => res.sendFile(path.join(__dirname, '../another.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+app.use("/another", (req, res) =>
+  res.sendFile(path.join(__dirname, "../another.html"))
+);
 
 module.exports = app;
 module.exports.handler = serverless(app);
